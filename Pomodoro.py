a0b25cdacf9d84fa562a570ee63d8cf850f59594
Hui Wen <xuwen0221@gmail.com>
2024-04-23 01:55:55 +0800

    edit: finalised start/stop/reset button placing.

--- Pomodoro.py
+++ Pomodoro.py
@@ -3,7 +3,7 @@ import time
 
 root = Tk()
 
-#Setting up Window's size and design
+#Setting up Window's size and design by defining Class
 class PomodoroTimer:
     def __init__(self, root):
         self.root = root
@@ -12,29 +12,29 @@ class PomodoroTimer:
         self.root.configure(bg = "IndianRed")
 
         self.frame = Frame(root, bg="IndianRed", height=50)
-        self.frame.place(relx=0.5, rely=1, anchor=S, relwidth=1)
+        self.frame.pack(side=BOTTOM, fill=X)
         
 
         #Timer Label Design [CENTRE]
-        self.timer_lbl = Label(root, text = "25:00", font= ("Times", 66), fg ="black", bg = "IndianRed")
+        self.timer_lbl = Label(root, text = "25:00", font= ("Times", 66,), fg ="black", bg = "IndianRed")
         self.timer_lbl.pack(pady=20)
         self.timer_lbl.place(relx=0.5, rely=0.4, anchor=CENTER)
 
         #Start Button Design
-        self.start_btn = Button(self.frame,text = "Start", font= ("Times", 20), fg = "black", activebackground = "grey", command=self.start_time)
-        self.start_btn.pack(side = LEFT)
+        self.start_btn = Button(self.frame,text = "Start", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.start_time)
+        self.start_btn.pack(side=LEFT, expand=True, fill=X)
 
         #Stop Button Design
-        self.stop_btn = Button(self.frame, text = "Stop", font= ("Times", 20), fg = "black", activebackground = "grey", command=self.pause_time)
-        self.stop_btn.pack(side = LEFT)
+        self.stop_btn = Button(self.frame, text = "Stop", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.pause_time)
+        self.stop_btn.pack(side=LEFT, expand=True, fill=X)
 
 
         #Reset Button Design
-        self.reset_btn = Button(self.frame, text = "Reset", font= ("Times", 20), fg = "black", activebackground = "grey", command=self.reset_time)
-        self.reset_btn.pack(side = LEFT)
+        self.reset_btn = Button(self.frame, text = "Reset", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.reset_time)
+        self.reset_btn.pack(side=LEFT, expand=True, fill=X)
 
         #Timer Variables
-        self.run_timer = FALSE #Haven't run yet
+        self.run_timer = FALSE #Timer haven't run yet
         self.start_timer = 0
         self.pause_timer = FALSE
         self.timer_duration = 1500 #25mins multiply by 60 seconds = 1500 seconds
