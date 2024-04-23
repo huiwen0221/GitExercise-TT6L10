4a279a31c2f5df15829b2c1996ab3e0b7fa7bcae
Hui Wen <xuwen0221@gmail.com>
2024-04-23 16:10:49 +0800

    edit: added timer end default alarm sound and extra comments, changed some variable names.

--- Pomodoro.py
+++ Pomodoro.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import time
+import winsound
 
 root = Tk()
 
@@ -33,24 +34,25 @@ class PomodoroTimer:
         self.reset_btn = Button(self.frame, text = "Reset", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.reset_time)
         self.reset_btn.pack(side=LEFT, expand=True, fill=X)
 
-        #Timer Variables
-        self.run_timer = FALSE #Timer haven't run yet
-        self.start_timer = 0
-        self.pause_timer = FALSE
-        self.timer_duration = 1500 #25mins multiply by 60 seconds = 1500 seconds
+        #Timer Variables (Original State)
+        self.run_timer = FALSE #Timer not running
+        self.start_timer = 0 #Initialized to 0
+        self.pause_timer = FALSE #Timer isn't paused
+        self.timer_duration = 1500 #25mins * 60 seconds = 1500 seconds
         self.remaining_time = self.timer_duration
 
     def start_time(self):
-        if not self.run_timer:
-            self.run_timer = TRUE
+        if not self.run_timer: #if the timer is not running,
+            self.run_timer = TRUE #change the timer to run and follow logic below.
             self.start_timer = time.time()
             self.update_time()
             self.start_btn.configure(state = DISABLED)
             self.stop_btn.configure(state= NORMAL)
     
+
     def pause_time(self):
-        if self.run_timer:
-            self.run_timer = FALSE
+        if self.run_timer: #if timer is running,
+            self.run_timer = FALSE #change it to not run.
             self.start_btn.configure(state = NORMAL)
             self.stop_btn.configure(state = DISABLED)
         
@@ -65,21 +67,26 @@ class PomodoroTimer:
     def update_time(self):
         if self.run_timer:
             current_time = time.time()
-            elapsed_time = current_time - self.start_timer
-            self.remaining_time = max(self.remaining_time - elapsed_time, 0)
+            time_passed = current_time - self.start_timer
+            self.remaining_time = max(self.remaining_time - time_passed, 0) #prevents remaining time from become less than 0
             self.update_display()
 
             if self.remaining_time > 0:
                 self.start_timer = current_time
-                self.root.after(1000, self.update_time)
-            else:
+                self.root.after(1000, self.update_time) #1000 = 1 second
+            else: #stop timer and play alarm sound
                 self.run_timer = False
+                self.alarm_sound()
     
     def update_display(self):
         minutes = int(self.remaining_time // 60)
         seconds = int(self.remaining_time % 60)
         time_str = "{:02d}:{:02d}".format(minutes, seconds)
         self.timer_lbl.config(text=time_str)
+
+    #Default Alarm Sound 
+    def alarm_sound(self):
+        winsound.PlaySound("default-timer-sound.wav", winsound.SND_FILENAME, winsound.SND_ASYNC)
             
 
 pomodoro_timer = PomodoroTimer(root)
