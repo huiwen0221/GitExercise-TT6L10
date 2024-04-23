172497fac40d9d457972644c31e2f0b7f81bc3ee
Nyiam Zi Qin <nyiamziqin@gmail.com>
2024-04-23 13:40:57 +0800

    label

--- Pomodoro.py
+++ Pomodoro.py
@@ -2,3 +2,13 @@ import tkinter as tk
 from tkinter import messagebox
 from tkcalendar import Calendar
 
+class StudyListApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Study List")
+        self.geometry("400x300")
+        self.configure(bg="light yellow")
+        
+        self.title_label = tk.Label(self, text="Study List", font=("Cooper Black", 50), bg="light yellow")  
+        self.title_label.pack(pady=10)
+
