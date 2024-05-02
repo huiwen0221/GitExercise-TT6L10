f79606d3ca00ecf9cad54a4a8235c6de50a861a4
Hui Wen <xuwen0221@gmail.com>
2024-05-02 11:10:25 +0800

    changed variable and function names

--- studymode.py
+++ studymode.py
@@ -26,102 +26,102 @@ class StudyTimer:
         self.study_timer_lbl.place(relx=0.5, rely=0.4, anchor=CENTER)
 
 #Study Mode Timer Button [TOP CENTRE]
-        self.study_timer_btn = Button(self.study_top_bar_frame, text = "Timer", font=("Times, 16"), fg = "black",activebackground = "grey", command=self.default_timer )
+        self.study_timer_btn = Button(self.study_top_bar_frame, text = "Timer", font=("Times, 16"), fg = "black",activebackground = "grey", command=self.study_default_timer )
         self.study_timer_btn.pack(side=TOP, padx=5, pady=10, anchor=CENTER)
 
 #Start Button Design [BOTTOM]
-        self.study_start_btn = Button(self.study_bottom_frame,text = "Start", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.start_time)
-        self.start_btn.pack(side=LEFT, expand=True, fill=X)
+        self.study_start_btn = Button(self.study_bottom_frame,text = "Start", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.study_start_time)
+        self.study_start_btn.pack(side=LEFT, expand=True, fill=X)
 
 #Stop Button Design [BOTTOM]
-        self.study_stop_btn = Button(self.study_bottom_frame, text = "Stop", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.pause_time)
+        self.study_stop_btn = Button(self.study_bottom_frame, text = "Stop", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.study_pause_time)
         self.study_stop_btn.pack(side=LEFT, expand=True, fill=X)
 
 #Reset Button Design [BOTTOM]
-        self.study_reset_btn = Button(self.study_bottom_frame, text = "Reset", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.reset_time)
+        self.study_reset_btn = Button(self.study_bottom_frame, text = "Reset", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.study_reset_time)
         self.study_reset_btn.pack(side=LEFT, expand=True, fill=X)
 
 #Short Break Button Design [BOTTOM]
-        self.study_shortbreak_btn = Button(self.study_bottom_frame, text = "Short Break", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.short_break )
+        self.study_shortbreak_btn = Button(self.study_bottom_frame, text = "Short Break", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.study_short_break )
         self.study_shortbreak_btn.pack(side=LEFT, expand=True, fill=X)
 
 #Long Break Button Design [BOTTOM]
-        self.study_longbreak_btn = Button(self.study_bottom_frame, text = "Long Break", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.long_break)
+        self.study_longbreak_btn = Button(self.study_bottom_frame, text = "Long Break", font= ("Times", 16), fg = "black", activebackground = "grey", command=self.study_long_break)
         self.study_longbreak_btn.pack(side=LEFT, expand=True, fill=X)
 
 #Timer Variables (Original State)
-        self.run_timer = FALSE #Timer not running
-        self.start_timer = 0 #Initialized to 0
-        self.pause_timer = FALSE #Timer isn't paused
-        self.timer_duration = 2700 #45mins * 60 seconds = 2700 seconds
-        self.remaining_time = self.timer_duration #set as same value to show that entire duration is remaining
+        self.study_run_timer = FALSE #Timer not running
+        self.study_start_timer = 0 #Initialized to 0
+        self.study_pause_timer = FALSE #Timer isn't paused
+        self.study_timer_duration = 2700 #45mins * 60 seconds = 2700 seconds
+        self.study_remaining_time = self.study_timer_duration #set as same value to show that entire duration is remaining
 
 #Default Timer Mode
-    def default_timer(self):
-        self.timer_duration = 2700
-        self.remaining_time = self.timer_duration
-        self.reset_time()
+    def study_default_timer(self):
+        self.study_timer_duration = 2700
+        self.study_remaining_time = self.study_timer_duration
+        self.study_reset_time()
 
 #Short Break
-    def short_break(self):
-        self.timer_duration = 900
-        self.remaining_time = self.timer_duration
-        self.reset_time()
+    def study_short_break(self):
+        self.study_timer_duration = 900
+        self.study_remaining_time = self.study_timer_duration
+        self.study_reset_time()
 
 #Long Break
-    def long_break(self):
-        self.timer_duration = 1500
-        self.remaining_time = self.timer_duration
-        self.reset_time()
+    def study_long_break(self):
+        self.study_timer_duration = 1500
+        self.study_remaining_time = self.study_timer_duration
+        self.study_reset_time()
 
 #Start Timer [ALL]
-    def start_time(self):
-        if not self.run_timer: #if the timer is not running,
-            self.run_timer = TRUE #change the timer to run.
-            self.start_timer = time.time()
-            self.update_time()
-            self.start_btn.configure(state = DISABLED)
-            self.stop_btn.configure(state= NORMAL)
+    def study_start_time(self):
+        if not self.study_run_timer: #if the timer is not running,
+            self.study_run_timer = TRUE #change the timer to run.
+            self.study_start_timer = time.time()
+            self.study_update_time()
+            self.study_start_btn.configure(state = DISABLED)
+            self.study_stop_btn.configure(state= NORMAL)
 
 #Pause Timer [ALL]
-    def pause_time(self):
-        if self.run_timer: #if timer is running,
-            self.run_timer = FALSE #change it to not run.
-            self.start_btn.configure(state = NORMAL)
-            self.stop_btn.configure(state = DISABLED)
+    def study_pause_time(self):
+        if self.study_run_timer: #if timer is running,
+            self.study_run_timer = FALSE #change it to not run.
+            self.study_start_btn.configure(state = NORMAL)
+            self.study_stop_btn.configure(state = DISABLED)
 
 #Reset Timer [ALL]
-    def reset_time(self):
-        self.run_timer = False
-        self.remaining_time = self.timer_duration #resetting timer to initial duration
-        self.start_btn.configure(state = NORMAL)
-        self.stop_btn.configure(state = DISABLED)
-        self.update_display()
+    def study_reset_time(self):
+        self.study_run_timer = False
+        self.study_remaining_time = self.study_timer_duration #resetting timer to initial duration
+        self.study_start_btn.configure(state = NORMAL)
+        self.study_stop_btn.configure(state = DISABLED)
+        self.study_update_display()
 
 #Updating time inside
-    def update_time(self):
-        if self.run_timer: #if timer is running,
-            current_time = time.time() #get current time and find the time passed since it started
-            time_passed = current_time - self.start_timer
-            self.remaining_time = max(self.remaining_time - time_passed, 0) #prevents remaining time from become less than 0
-            self.update_display()
+    def study_update_time(self):
+        if self.study_run_timer: #if timer is running,
+            study_current_time = time.time() #get current time and find the time passed since it started
+            study_time_passed = study_current_time - self.study_start_timer
+            self.study_remaining_time = max(self.study_remaining_time - study_time_passed, 0) #prevents remaining time from become less than 0
+            self.study_update_display()
 
-            if self.remaining_time > 0:
-                self.start_timer = current_time
-                self.root.after(1000, self.update_time) #1000 = 1 second, updates after 1 second
+            if self.study_remaining_time > 0:
+                self.study_start_timer = study_current_time
+                self.root.after(1000, self.study_update_time) #1000 = 1 second, updates after 1 second
             else: 
-                self.run_timer = False
-                self.alarm_sound()
+                self.study_run_timer = False
+                self.study_alarm_sound()
 
 #Updating time outside and displaying it
-    def update_display(self):
-        minutes = int(self.remaining_time // 60)
-        seconds = int(self.remaining_time % 60)
+    def study_update_display(self):
+        minutes = int(self.study_remaining_time // 60)
+        seconds = int(self.study_remaining_time % 60)
         time_str = "{:02d}:{:02d}".format(minutes, seconds)
-        self.timer_lbl.config(text=time_str)
+        self.study_timer_lbl.config(text=time_str)
 
 #Default Alarm Sound 
-    def alarm_sound(self):
+    def study_alarm_sound(self):
         winsound.PlaySound("default-timer-sound.wav", winsound.SND_FILENAME, winsound.SND_ASYNC)
             
 
